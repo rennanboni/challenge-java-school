b809db670c909f95afe2117c6866be32108472de
renna <renna@host.docker.internal>
2025-07-31 04:47:28 -0300

    initial commit

--- src/main/java/com/challenge/school/mapper/SchoolMapper.java
+++ src/main/java/com/challenge/school/mapper/SchoolMapper.java
@@ -3,7 +3,6 @@ package com.challenge.school.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.factory.Mappers;
 
 import com.challenge.school.domain.School;
 import com.challenge.school.dto.SchoolCreateDto;
@@ -12,12 +11,12 @@ import com.challenge.school.dto.SchoolUpdateDto;
 
 @Mapper(componentModel = "spring")
 public interface SchoolMapper {
-	SchoolMapper INSTANCE = Mappers.getMapper(SchoolMapper.class);
 
-    @Mapping(target = "students", ignore = true)
     SchoolDto toDto(School entity);
+    @Mapping(target = "students", ignore = true)
+    SchoolDto toDtoWithoutStudents(School entity);
     
-    public abstract School toEntity(SchoolCreateDto dto);
+    School toEntity(SchoolCreateDto dto);
 
-    public abstract void updateFromDto(SchoolUpdateDto dto, @MappingTarget School entity);
+    void updateFromDto(SchoolUpdateDto dto, @MappingTarget School entity);
 }
--- src/main/java/com/challenge/school/mapper/StudentMapper.java
+++ src/main/java/com/challenge/school/mapper/StudentMapper.java
@@ -8,16 +8,15 @@ import com.challenge.school.domain.Student;
 import com.challenge.school.dto.StudentCreateDto;
 import com.challenge.school.dto.StudentDto;
 import com.challenge.school.dto.StudentUpdateDto;
-import com.challenge.school.repository.SchoolRepository;
 
-@Mapper(componentModel = "spring", uses = { SchoolRepository.class })
-public abstract class StudentMapper {
+@Mapper(componentModel = "spring")
+public interface StudentMapper {
 
 	@Mapping(source = "school.id", target = "schoolId")
-	public abstract StudentDto toDto(Student entity);
+	StudentDto toDto(Student entity);
 
     @Mapping(source = "schoolId", target = "school.id")
-    public abstract Student toEntity(StudentCreateDto dto);
+    Student toEntity(StudentCreateDto dto);
 
-    public abstract void updateFromDto(StudentUpdateDto dto, @MappingTarget Student entity);
+    void updateFromDto(StudentUpdateDto dto, @MappingTarget Student entity);
 }
